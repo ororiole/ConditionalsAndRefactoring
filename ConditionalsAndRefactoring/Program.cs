7184cb901f0dfb1734451103a09eb1f1d8b8901a
ororiole <ororiole@hotmail.com>
2020-06-30 15:11:22 -0600

    first commit

---
+++ ConditionalsAndRefactoring/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using FeatureFactory;
+
+namespace ConditionalsAndRefactoring
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			var ooc = new OutOfControlConditionals();
+			ooc.ProcessData();
+
+			Console.WriteLine();
+
+			var cf = new CleanRefactor();
+			cf.ProcessData();
+
+		}
+	}
+
+	public static class MockDataSet
+	{
+		public static List<Feature> list = new List<Feature> 
+		{
+			new Feature {FeatureType = 1, Status = 2},	
+			new Feature {FeatureType = 2, Status = 1},
+			new Feature {FeatureType = 3, Status = 1}
+		};
+	}
+
+	public class OutOfControlConditionals				// Each class would be in its own file of course but the example here is to illustrate
+	{													// a handy refactoring, not to make you read thru a dozen files.
+		public void ProcessData()
+		{
+			foreach(Feature f in MockDataSet.list)
+			{
+				if(f.FeatureType == 1 && f.Status != 2)
+				{
+					//do some processing
+					Console.WriteLine( "Processing FeatureType {0}", f.FeatureType );
+				}
+				else if (f.FeatureType == 3 && (f.Status == 1 || f.Status == 3))
+				{
+					//do the 3 processing
+					Console.WriteLine( "Processing FeatureType {0}", f.FeatureType );
+				}
+				else
+				{
+					//do default processing
+					Console.WriteLine( "Processing Default FeatureType {0}", f.FeatureType );
+				}
+			}
+			Console.WriteLine( "Note FeatureType 1 and 2 are both handled as Defaults, is that even what was intended?" );
+		}
+	}
+
+	public class CleanRefactor
+	{
+		public void ProcessData()
+		{
+			foreach (Feature f in MockDataSet.list)
+			{
+				IFeature myFeature = MyFeatureFactory.GetFeature( f );
+				myFeature.DoProcess();
+			}
+		}
+	}
+
+}
---
+++ FeatureFactory/Feature.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace FeatureFactory
+{
+    public class Feature
+    {
+		public int FeatureType { get; set; }	// I would normally make these enums but again for the purpose
+												// of reviewing a sample lets not make
+												// someone read dozens of files just for an illustration
+		public int Status { get; set; }
+    }
+
+	public interface IFeature
+	{
+		void DoProcess();
+		int Status { get; set; }
+		int FeatureType { get; set; }
+	}
+
+	public class Feature1 : IFeature
+	{
+		public Feature1(int status)
+		{
+			this.Status = status;
+			FeatureType = 1;
+		}
+		#region IFeature members
+		public void DoProcess()
+		{
+			if (Status != 2)	// the single responsibility for status moves inside the interface implementation
+			{
+				//1 is NOT the default, so now it is handled properly. In this case, it is not processed.
+				Console.WriteLine( "Processing FeatureType {0}", FeatureType );
+			}
+		}
+		public int Status { get; set; }
+		public int FeatureType { get; set; }
+		#endregion
+	}
+
+	public class Feature2 : IFeature
+	{
+		public Feature2(int status)
+		{
+			this.Status = status;
+			FeatureType = 2;
+		}
+
+		#region IFeature members
+		public void DoProcess()
+		{
+			//2 is the default, so it is handled properly
+			Console.WriteLine( "Processing Default FeatureType {0}", FeatureType );
+		}
+		public int Status { get; set; }
+		public int FeatureType { get; set; }
+		#endregion
+	}
+
+	public class Feature3 : IFeature
+	{
+		public Feature3(int status)
+		{
+			this.Status = status;
+			FeatureType = 3;
+		}
+
+		#region IFeature members
+		public void DoProcess()
+		{
+			//3 is also handled properly, NOT the default if its status is 2
+			if (Status == 1 || Status == 3)
+			{
+				Console.WriteLine( "Processing FeatureType {0}", FeatureType );
+			}
+		}
+		public int Status { get; set; }
+		public int FeatureType { get; set; }
+		#endregion
+	}
+
+	public static class MyFeatureFactory
+	{
+		public static IFeature GetFeature(Feature datasetFeature)
+		{
+			switch (datasetFeature.FeatureType)
+			{
+				case 1: return new Feature1( datasetFeature.Status );
+
+				case 3: return new Feature3( datasetFeature.Status );
+
+				default: return new Feature2( datasetFeature.Status );
+			}
+		}
+	}
+
+
+}
